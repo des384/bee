9529e25ad64583ca67f921a09f7866ddd95b1c72
des384 <75841716+des384@users.noreply.github.com>
2020-12-19 21:23:03 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(function(){
 
-    $('.menu__btn').on('click', function (){
-        $('.menu__list').toggleClass('menu__list--active')
-      }); 
+    // $('.menu__btn').on('click', function (){
+    //     $('.menu__list').toggleClass('menu__list--active')
+    //   });     
 
-        
+    $('.menu__btn').on('click', function(){
+      $('.menu__list').slideToggle();
+     });
+    
+
 });
